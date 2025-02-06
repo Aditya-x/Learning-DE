874bfae03b63ad4dc7f50b019d28b3edb6ad51e2
Aditya Kumar <54754910+Aditya-x@users.noreply.github.com>
2025-02-06 16:58:39 +0530

    Airflow GUI exploration

--- Airflow/dags/data_ingestion_localdb.py
+++ Airflow/dags/data_ingestion_localdb.py
@@ -2,25 +2,34 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
+import os
+
 from datetime import datetime
 
 local_workflow = DAG(
     "LocalIngestionDAG",
     schedule_interval="0 6 2 * *",
-    start_date= datetime(2025,3,3)
+    start_date= datetime(2025,1,1)
 )
 
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
+URL_PREFIX = "https://d37ci6vzurychx.cloudfront.net/trip-data"
+month = 01
+URL_TEMPLATE = URL_PREFIX + F'yellow_tripdata_2021-{month}.parquet'
+
+URL = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet'
 with local_workflow:
 
     wget_task = BashOperator(
         task_id='wget',
-        bash_command='echo "Hello World!"'
+        bash_command=f'curl -sSL {URL} > {AIRFLOW_HOME}/output.parquet'
     )
+
 
     ingest_task = BashOperator(
         task_id='ingest',
-        bash_command='pwd'
+        bash_command=f'ls {AIRFLOW_HOME}'   
     )
 
 
